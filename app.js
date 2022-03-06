1d9aa77c0dc2b0710baadbda31d249acaad21221
Sourabh Shukla <82205268+anonymous372@users.noreply.github.com>
2022-03-06 11:09:01 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,86 +3,14 @@ const app = express()
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const mongoose = require('mongoose')
 
-// var json = {
-//     "btcinr": {
-//     "base_unit": "btc",
-//     "quote_unit": "inr",
-//     "low": "3065101.0",
-//     "high": "3320313.0",
-//     "last": "3106104.0",
-//     "type": "SPOT",
-//     "open": "3286003",
-//     "volume": "299.46722",
-//     "sell": "3105676.0",
-//     "buy": "3103906.0",
-//     "at": 1646471423,
-//     "name": "BTC/INR"
-//     },
-//     "xrpinr": {
-//     "base_unit": "xrp",
-//     "quote_unit": "inr",
-//     "low": "55.3216",
-//     "high": "58.6999",
-//     "last": "57.934",
-//     "type": "SPOT",
-//     "open": "58.1405",
-//     "volume": "593121.4",
-//     "sell": "57.9342",
-//     "buy": "57.8852",
-//     "at": 1646471423,
-//     "name": "XRP/INR"
-//     },
-//     "ethinr": {
-//     "base_unit": "eth",
-//     "quote_unit": "inr",
-//     "low": "205000.0",
-//     "high": "217962.9",
-//     "last": "210071.8",
-//     "type": "SPOT",
-//     "open": "216510.1",
-//     "volume": "589.5778",
-//     "sell": "210071.7",
-//     "buy": "209820.9",
-//     "at": 1646471423,
-//     "name": "ETH/INR"
-//     },
-//     "trxinr": {
-//     "base_unit": "trx",
-//     "quote_unit": "inr",
-//     "low": "4.58",
-//     "high": "4.7631",
-//     "last": "4.7222",
-//     "type": "SPOT",
-//     "open": "4.6329",
-//     "volume": "10921845.0",
-//     "sell": "4.7222",
-//     "buy": "4.722",
-//     "at": 1646471423,
-//     "name": "TRX/INR"
-//     },
-//     "eosinr": {
-//     "base_unit": "eos",
-//     "quote_unit": "inr",
-//     "low": "156.85",
-//     "high": "169.98",
-//     "last": "160.14",
-//     "type": "SPOT",
-//     "open": "163.05",
-//     "volume": "8330.24",
-//     "sell": "160.16",
-//     "buy": "159.51",
-//     "at": 1646471423,
-//     "name": "EOS/INR"
-//     },
-// }
-
+const port =  process.env.PORT || 5000
 app.use(express.static("views"))
 app.use(express.static("public"))
 app.set('view engine', 'ejs')
 
 // DB Config
 var Crypto = require('./models/crypto')
-const url = "mongodb+srv://ace:<password>@cluster0.a5ivo.mongodb.net/Crypto?retryWrites=true&w=majority"
+const url = process.env.MONGODB_URL
 
 mongoose.connect(
     url,
@@ -110,7 +38,7 @@ app.get("/index", function(req,res){
     })
 })
 
-app.listen(3000, function(){
+app.listen(port, function(){
     console.log("Listening on Port : 3000")
 })
 
